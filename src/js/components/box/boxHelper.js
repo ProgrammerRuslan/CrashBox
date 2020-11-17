2951982fb68278ff33ac9011ad223a5f89b38d27
ProgrammerRuslan <vos.nau.04@gmail.com>
2020-11-17 18:04:15 +0200

    removed the function deleteBoxes() and added its logic to the function createBoxes()

--- src/js/components/box/boxHelper.js
+++ src/js/components/box/boxHelper.js
@@ -2,26 +2,21 @@ import Box from './box'
 
 function createBoxes(count){
   let boxes = []
-
+  let score = 0
   for (let i = 0; i < count; i++) {
     boxes[i] = new Box(i)
     boxes[i].addBox()
+    console.log(boxes[i])
+    boxes[i].name.addEventListener("click", () => {
+      boxes[i].name.style.display = 'none'
+      score++
+      document.querySelector('.score').innerHTML = 'SCORE: ' + score
+    })
   }
 
   return boxes
 }
 
-function deleteBoxes(){
-  let boxes = document.querySelectorAll('.box')
-  let score = 0
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].onclick = function () {
-      boxes[i].style.display = 'none'
-      score++
-      document.querySelector('.score').innerHTML = 'SCORE: ' + score
-    }
-  }
-}
+
 
 export {createBoxes}
-export {deleteBoxes}
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,14 @@
 import {createBoxes} from './components/box/boxHelper'
-import {deleteBoxes} from './components/box/boxHelper'
+
 import '../css/main.css'
 
 let boxes = createBoxes(10)
-
-deleteBoxes()
 
 setInterval(() => {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].move()
   }
-}, 10)
+}, 20)
 
 
 
