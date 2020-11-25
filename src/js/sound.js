364d15e122dbcf27be21915ce45b1d1ecd67d688
Andrew <vozniukandrey@gmail.com>
2020-11-25 19:58:29 +0200

    delete file name

--- src/js/sound.js
+++
@@ -1,2 +0,0 @@
-export const mainSound = new Audio('https://dl.vgmdownloads.com/soundtracks/geometry-dash/dwjmrcyg/01.Forever%20Bound%20-%20Stereo%20Madness.mp3')
-export const crashBox = new Audio('https://dl.vgmdownloads.com/soundtracks/bomb-jack-arcade/fyccmdrqhg/02%20Credit.mp3')
